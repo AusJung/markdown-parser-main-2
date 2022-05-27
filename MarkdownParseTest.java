52f16c7bdac9500e0bc15a2c6a9a01d70e09e5d6
AusJung <aujung@ucsd.edu>
2022-05-27 16:48:35 -0700

    commit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -46,4 +46,74 @@ public class MarkdownParseTest {
         }
         
     }
+    @Test
+    public void getLinksForSnippet1() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet1.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("url.com");
+            actual.add("`google.com");
+            actual.add("google.com");
+            actual.add("ucsd.edu");
+            assertEquals(actual.toString(), links.toString());
+    }
+    @Test
+    public void getLinksForSnippet2() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet2.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("a.com");
+            actual.add("a.com(())");
+            actual.add("example.com");
+            assertEquals(actual.toString(), links.toString());
+    }
+    @Test
+    public void getLinksForSnippet3() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet3.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("https://www.twitter.com ");
+            actual.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+            actual.add("github.com");
+            actual.add("https://cse.ucsd.edu/");
+            assertEquals(actual.toString(), links.toString());
+    }
+    @Test
+    public void getLinksForSnippet1Review() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet1.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParseReview.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("url.com");
+            actual.add("`google.com");
+            actual.add("google.com");
+            actual.add("ucsd.edu");
+            assertEquals(actual.toString(), links.toString());
+    }
+    @Test
+    public void getLinksForSnippet2Review() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet2.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParseReview.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("a.com");
+            actual.add("a.com(())");
+            actual.add("example.com");
+            assertEquals(actual.toString(), links.toString());
+    }
+    @Test
+    public void getLinksForSnippet3Review() throws IOException{
+        Path fileName = Path.of("/Users/austin/Downloads/markdown-parser-main 2/labReport4Snippet3.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParseReview.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("https://www.twitter.com ");
+            actual.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+            actual.add("github.com");
+            actual.add("https://cse.ucsd.edu/");
+            assertEquals(actual.toString(), links.toString());
+    }
 }
